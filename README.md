efcb771a552a832ef42a4ab31cecf8fdb5f2d9fb
choushruti88 <chou.shruti@gmail.com>
2018-05-01 22:49:51 -0700

    Graduate Assignment Submission

---
+++ README.md
@@ -0,0 +1,6 @@
+# chattrbox_GradAssig
+Graduate Assignment
+
+* File path was being extracted with extract.js, it is removed now.
+
+* Diet.js is installed and functions under diet and Diet-static module used to fetch url and handle error for non-existing or invalid paths.
---
+++ index.js
@@ -0,0 +1,39 @@
+/* Shruti Choudhary */
+var server = require("diet");
+var ws = require("./websockets-server");
+var app = server();
+var fs = require("fs");
+// HTTP listen with a URL String to port 8000
+app.listen("http://localhost:8000");
+
+// Require the diet-static module and configure it
+var diet_static_m = require("diet-static")({
+  path: app.path + "/app/"
+});
+
+// Attach the static module as a footer middleware
+app.footer(diet_static_m);
+app.view("file", diet_static_m);
+
+//to get index page through redirect function
+app.get("/", function($) {
+  $.redirect("index.html");
+});
+
+//missing function is to handle the case when tried page is not accessible
+app.missing(function($) {
+  $.header("content-type", "text/html");
+
+  $.status("404", "File not found"); // -> 404 not found
+
+  fs.readFile(__dirname + "/app/error.html", function(err, data) {
+    if (err) throw err;
+    $.end(data.toString());
+  });
+});
+
+// Subscribe to all error events; callback which we want to run on callback
+app.error(function($, middleware) {
+  // Log the error
+  console.trace("Something bad happened....!!", $.status, $.error.message);
+});
